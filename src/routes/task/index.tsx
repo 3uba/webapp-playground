5892944d339798043026cee3e75769378d53e196
Jakub <jakubik.jb.40@gmail.com>
2023-12-16 17:09:49 +0100

    task-2: working modal

--- src/routes/task/index.tsx
+++ src/routes/task/index.tsx
@@ -1,7 +1,35 @@
-import {component$, useStyles$} from "@builder.io/qwik";
+import {component$, $} from "@builder.io/qwik";
+import {createWeb3Modal, defaultConfig} from "@web3modal/ethers5";
+
+const projectId = import.meta.env.PUBLIC_WALLET_CONNECT_KEY;
+
+const mainnet = {
+    chainId: 1,
+    name: 'Ethereum',
+    currency: 'ETH',
+    explorerUrl: 'https://etherscan.io',
+    rpcUrl: 'https://cloudflare-eth.com'
+}
+
+const metadata = {
+    name: 'My Test Website',
+    description: 'Test',
+    url: 'https://test.com',
+    icons: ['https://avatars.test.com/']
+}
+
+const modal = createWeb3Modal({
+    ethersConfig: defaultConfig({ metadata }),
+    chains: [mainnet],
+    projectId
+});
 
 export default component$(() => {
+    const open = $(() => modal.open())
+
     return (
-        <div class={"bg-black h-screen"}></div>
+        <div class={" h-screen"}>
+            <w3m-button onClick$={open}>Connect</w3m-button>
+        </div>
     );
 });
